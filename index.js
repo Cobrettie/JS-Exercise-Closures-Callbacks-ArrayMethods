7ac7c1ea140d73399e15953cccf01014120516f3
Cobrettie <cobrettiegarner3@gmail.com>
2020-01-18 12:46:59 -0500

    complete firstNamesAllCaps challenge

--- index.js
+++ index.js
@@ -205,8 +205,14 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
+function firstNamesAllCaps(runners) {
   /* CODE HERE */
+  const allCapNames = runners.map((name) => {
+    const capFirstName = name.first_name.toUpperCase();
+    return `${capFirstName}`;
+  });
+
+  return allCapNames;
 }
 
 /**
